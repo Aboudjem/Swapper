2a8d72d66246218dc32bfac7d4d8eefda82ea522
Adam Boudj <boudjemaa.adam@gmail.com>
2023-01-20 19:37:54 +0400

    :art: optimize and change require message

--- contracts/Swapper.sol
+++ contracts/Swapper.sol
@@ -82,7 +82,7 @@ contract Swapper {
     function _swap(address token_, uint256 amount) private {
         require(
             IERC20(token_).transferFrom(msg.sender, address(this), amount),
-            "ERC20: Error"
+            "ERC20: Error on transfer"
         );
         require(tokenC.transfer(msg.sender, amount), "Error");
     }
@@ -95,17 +95,9 @@ contract Swapper {
      */
     function _unswap(address token_, uint256 amount) private {
         require(
-            (tokenC.allowance(msg.sender, address(this)) >= amount),
-            "ERC20: transfer amount exceeds allowance"
-        );
-        require(
-            IERC20(token_).balanceOf(address(this)) >= amount,
-            "ERC20: transfer amount exceeds balance"
-        );
-        require(
             tokenC.transferFrom(msg.sender, address(this), amount),
-            "ERC20: Error"
+            "ERC20: Error on transfer"
         );
-        require(IERC20(token_).transfer(msg.sender, amount), "Error");
+        require(IERC20(token_).transfer(msg.sender, amount), "ERC20: Error on transfer");
     }
 }
